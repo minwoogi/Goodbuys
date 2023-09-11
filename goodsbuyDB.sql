2858be567d5ba97175de7becaf667e0b26eaae80
minwoogi <csc1925@naver.com>
2023-09-11 16:43:59 +0900

    docs: Update ERD

--- goodsbuyDB.sql
+++ goodsbuyDB.sql
@@ -25,6 +25,7 @@ create table member_profile
     introduction TEXT,
     image_url    VARCHAR(200),
     birthday     VARCHAR(128),
+    location     varchar(20) NOT NULL,
     PRIMARY KEY (profile_id),
     FOREIGN KEY (user_no) REFERENCES member_user (user_no)
         ON DELETE CASCADE
@@ -33,18 +34,22 @@ create table member_profile
 alter table member_profile
     auto_increment = 1;
 
+DROP TABLE IF EXISTS product;
+
 create table product
 (
     product_no        BIGINT AUTO_INCREMENT,
+    user_no           BIGINT      NOT NULL,
+    nickname          VARCHAR(12) NOT NULL,
     product_name      VARCHAR(20) NOT NULL,
     product_image_url VARCHAR(2083),
-    product_price     INT NOT NULL,
-    locaiton_no       TINYINT NOT NULL,
-    PRIMARY KEY (product_no)
+    product_price     INT         NOT NULL,
+    location          varchar(20) NOT NULL,
+    category_no       TINYINT     NOT NULL,
+    PRIMARY KEY (product_no),
+    FOREIGN KEY (user_no) REFERENCES member_user (user_no)
+        ON DELETE CASCADE
 );
 
-ALTER TABLE product AUTO_INCREMENT=1;
-
-
-
-
+ALTER TABLE product
+    AUTO_INCREMENT = 1;
